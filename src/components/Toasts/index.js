6e60da0bda91f1daa38e8b5ca555705d1c45ac6e
Nguyễn Anh Tuấn <anhtuank7c@hotmail.com>
2017-10-15 21:52:30 +0700

    Update toast

--- src/components/Toasts/index.js
+++ src/components/Toasts/index.js
@@ -1,20 +1,23 @@
 import React, { PureComponent } from 'react';
-import { View, Modal, TouchableOpacity } from 'react-native';
-import { Text, Button } from 'native-base';
 import { connect } from 'react-redux';
+import { View, TouchableOpacity, BackHandler } from 'react-native';
+import { Text, Button } from 'native-base';
 // for convenient, we can just import one
 import { clearToast } from '~/store/actions/common';
 import { getToast } from '~/store/selectors/common';
+import material from '~/theme/variables/material';
 
-@connect(
-  state => ({
-    toast: getToast(state)
-  }),
-  { clearToast }
-)
+@connect(state => ({ toast: getToast(state) }), { clearToast })
 export default class extends PureComponent {
-  componentWillMount() {
-    clearTimeout(this.timer);
+  constructor(props) {
+    super(props);
+
+    this.hander = BackHandler.addEventListener('hardwareBackPress', () => this._closeToast(100));
+  }
+
+  componentWillUnmount() {
+    this.timer && clearTimeout(this.timer);
+    BackHandler.removeEventListener(this.hander);
   }
 
   _closeToast = duration => {
@@ -27,9 +30,7 @@ export default class extends PureComponent {
   renderToastMessage(message, levelProps) {
     return (
       <Button full iconRight {...levelProps} onPress={() => this._closeToast(100)}>
-        <Text style={{ color: '#fff' }}>
-          {message}
-        </Text>
+        <Text style={{ color: '#fff' }}>{message}</Text>
       </Button>
     );
   }
@@ -48,31 +49,47 @@ export default class extends PureComponent {
   render() {
     // we can display close all or something
     // for this to show toast only when cross form, for update call Toast.show directly
-    if (!this.props.toast) return false;
+    if (!this.props.toast) return null;
     const { position, message, level, duration } = this.props.toast;
     this._closeToast(duration);
     const levelProps = { [level]: true };
-    const justifyContent =
-      position === 'top'
-        ? 'flex-start'
-        : position === 'bottom' ? 'flex-end' : position === 'center' ? 'center' : 'flex-start';
+    let justify;
+    switch (position) {
+      case 'top':
+        justify = {
+          left: 0,
+          right: 0,
+          top: 0
+        };
+        break;
+      case 'center':
+        justify = {
+          left: 0,
+          right: 0,
+          top: material.deviceHeight / 2
+        };
+        break;
+      default:
+        justify = {
+          left: 0,
+          right: 0,
+          bottom: 0
+        };
+        break;
+    }
     return (
-      <Modal
-        animationType={position === 'bottom' ? 'slide' : 'fade'}
-        transparent
-        onRequestClose={() => this._closeToast(100)}
+      <View
+        style={{
+          position: 'absolute',
+          ...justify,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
       >
-        <View
-          style={{
-            flex: 1,
-            justifyContent
-          }}
-        >
-          {typeof message === 'string'
-            ? this.renderToastMessage(message, levelProps)
-            : this.renderToastView(message)}
-        </View>
-      </Modal>
+        {typeof message === 'string'
+          ? this.renderToastMessage(message, levelProps)
+          : this.renderToastView(message)}
+      </View>
     );
   }
 }
